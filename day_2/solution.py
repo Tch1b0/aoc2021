6406967929992938bd86e01cab4d4b2511073861
tch1b0 <jpour3006@gmail.com>
2021-12-02 06:13:58 +0100

    finish day 2

---
+++ day_2/solution.py
@@ -0,0 +1,55 @@
+from typing import Match
+
+
+def get_input() -> str:
+    """
+    Returns the input of the input.txt file
+    """
+    txt = ""
+    with open("day_2/input.txt", "r") as f:
+        txt = f.read()
+    
+    return txt.split("\n")
+
+def output(string1: str = "None", string2: str = "None") -> None:
+    """
+    Write the solution to both parts into the output file
+    """
+    with open("day_2/output.txt", "w") as f:
+        f.write(f"Part 1:\n{string1}\n=========\nPart 2:\n{string2}")
+
+def part1(commands: list[str]) -> str:
+    depth = 0
+    h = 0
+    for command in commands:
+        command = command.split()
+        if command[0] == "forward":
+            h += int(command[1])
+        elif command[0] == "up":
+            depth -= int(command[1])
+        elif command[0] == "down":
+            depth += int(command[1])
+    
+    return depth * h
+
+def part2(commands: list[str]) -> str:
+    depth = 0
+    h = 0
+    aim = 0
+    for command in commands:
+        command = command.split()
+        if command[0] == "forward":
+            h += int(command[1])
+            depth += aim * int(command[1])
+        elif command[0] == "up":
+            aim -= int(command[1])
+        elif command[0] == "down":
+            aim += int(command[1])
+    
+    return depth * h
+
+if __name__ == "__main__":
+    output(
+        part1(get_input()),
+        part2(get_input())
+    )
