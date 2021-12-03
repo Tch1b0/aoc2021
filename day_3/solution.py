e1e4aa33713542272a7fe560afe6630f2302d79b
Tch1b0 <Jpour3006@gmail.com>
2021-12-03 13:00:41 +0100

    Solve Part 2 of day 3

--- day_3/solution.py
+++ day_3/solution.py
@@ -18,18 +18,44 @@ def output(string1: str = "None", string2: str = "None") -> None:
 def part1(bits: list[str]) -> str:
     gamma_rate = ""
     for i in range(0, len(bits[0])):
-        current_bit = [bit[i] for bit in bits]
-        if current_bit.count("1") > current_bit.count("0"):
+        bit_parts = [bit[i] for bit in bits]
+        if bit_parts.count("1") > bit_parts.count("0"):
             gamma_rate += "1"
         else:
             gamma_rate += "0"
     return int(gamma_rate, 2) * int(gamma_rate.replace("1","!").replace("0","1").replace("!","0"), 2)
 
-def part2() -> str:
-    pass
+def part2(all_bits: list[str]) -> str:
+    bits = all_bits.copy()
+
+    for i in range(0, len(bits[0])):
+        if len(bits) == 1:
+            break
+        bit_parts = [bit[i] for bit in bits]
+        if bit_parts.count("1") >= bit_parts.count("0"):
+            bits = [bit for bit in bits if bit[i] == "1"]
+        else:
+            bits = [bit for bit in bits if bit[i] == "0"]
+    
+    oxygen = bits[0]
+    bits = all_bits.copy()
+
+    for i in range(0, len(bits[0])):
+        if len(bits) == 1:
+            break
+        bit_parts = [bit[i] for bit in bits]
+        if bit_parts.count("1") >= bit_parts.count("0"):
+            bits = [bit for bit in bits if bit[i] == "0"]
+        else:
+            bits = [bit for bit in bits if bit[i] == "1"]
+
+    co2 = bits[0]
+
+    return int(oxygen, 2) * int(co2, 2)
 
 if __name__ == "__main__":
+    i = get_input()
     output(
-        part1(get_input()),
-        part2()
+        part1(i),
+        part2(i)
     )
