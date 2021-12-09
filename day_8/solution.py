f9e14a30dc07325b43443ffdb24fcefc814349cd
Tch1b0 <Jpour3006@gmail.com>
2021-12-09 18:20:32 +0100

    attempt part 2

--- day_8/solution.py
+++ day_8/solution.py
@@ -15,6 +15,28 @@ def output(string1: str = "None", string2: str = "None") -> None:
     with open("day_8/output.txt", "w") as f:
         f.write(f"Part 1:\n{string1}\n=========\nPart 2:\n{string2}")
 
+def bcd_to_int(
+    code: str, 
+    rel: dict[str, str] = {"a": "a", "b": "b", "c": "c", "d": "d", "e": "e", "f": "f", "g": "g"}
+    ) -> int:
+    segments = [rel[char] for char in code]
+    number_codes: list[list[str]] = [
+        ["a", "b", "c", "e", "f", "g"],
+        ["c", "f"],
+        ["a", "c", "d", "e", "g"],
+        ["a", "c", "d", "f", "g"],
+        ["b", "c", "d", "f"],
+        ["a", "b", "d", "f", "g"],
+        ["a", "b", "d", "e", "f", "g"],
+        ["a", "c", "f"],
+        ["a", "b", "c", "d", "e", "f", "g"],
+        ["a", "b", "c", "d", "f", "g"]
+    ]
+    return number_codes.index(segments)
+
+def decrypt_segment(segment: list[str]):
+    pass 
+
 def part1(segments: list[list[str]]) -> int:
     p = 0
     for segment in segments:
@@ -24,11 +46,12 @@ def part1(segments: list[list[str]]) -> int:
 
     return p
 
-def part2() -> str:
-    pass
+def part2(segments) -> str:
+    for segment in segments:
+        decrypt_segment(segment)
 
 if __name__ == "__main__":
     output(
         part1(get_input()),
-        part2()
+        part2(get_input())
     )
