437c7ea102b616cfc513ccffc039ca58bb497831
Tch1b0 <Jpour3006@gmail.com>
2022-02-03 17:10:48 +0100

    visualize output

--- day_13/solution.py
+++ day_13/solution.py
@@ -19,7 +19,7 @@ def get_input(example: bool = False) -> str:
                 content["instructions"].append(
                     line.replace("fold along ", "").split("="))
             else:
-                coord = Coordinates(*[int(x) for x in line.split(",")])
+                coord = Coordinate(*[int(x) for x in line.split(",")])
                 content["points"].append(coord)
 
     return content
@@ -33,7 +33,7 @@ def output(string1: str = "None", string2: str = "None") -> None:
         f.write(f"Part 1:\n{string1}\n=========\nPart 2:\n{string2}")
 
 
-class Coordinates():
+class Coordinate():
     x: int
     y: int
 
@@ -48,23 +48,29 @@ class Coordinates():
         return hash((self.x, self.y))
 
     def __eq__(self, other):
-        if type(other) is Coordinates:
+        if type(other) is Coordinate:
             return self.x == other.x and self.y == other.y
         else:
             raise ValueError()
 
 
 class Paper():
-    points: list[Coordinates] = []
+    points: list[Coordinate] = []
 
-    def __init__(self, points: list[Coordinates] | list[tuple[int, int]] = None) -> None:
+    def __init__(self, points: list[Coordinate] | list[tuple[int, int]] = None) -> None:
         if points:
             for point in points:
                 self.add_point(point)
 
-    def add_point(self, coords: Coordinates | tuple[int, int]) -> None:
+    def contains_point(self, coords: Coordinate | tuple[int, int]) -> bool:
+        if type(coords) is not Coordinate:
+            coords = Coordinate(*coords)
+
+        return coords in self.points
+
+    def add_point(self, coords: Coordinate | tuple[int, int]) -> None:
         if type(coords) is tuple:
-            coords = Coordinates(*coords)
+            coords = Coordinate(*coords)
 
         self.points.append(coords)
 
@@ -81,20 +87,27 @@ class Paper():
                     point.x -= diff*2 - (1 if diff != 1 else 0)
 
 
-def part1(inp: dict[str, Any]) -> int:
+def part1(inp: dict[str, Any], verbose: bool = True) -> int:
     paper = Paper(inp["points"])
     for axis, ordinate in inp["instructions"]:
+        if verbose:
+            max_x = sorted(paper.points, key=lambda x: x.x)[-1].x
+            max_y = sorted(paper.points, key=lambda x: x.y)[-1].y
+            for i in range(max_y):
+                for j in range(max_x):
+                    print("#" if paper.contains_point((j, i)) else ".", end="")
+                print()
+            print()
         paper.fold(axis, int(ordinate))
 
     return len(set(paper.points))
 
-
-def part2(_) -> str:
+def part2(_) -> None:
     pass
 
 
 if __name__ == "__main__":
     output(
-        part1(get_input()),
+        part1(get_input(), False),
         part2(get_input())
     )
